a2ccb3a980ca005ea4b0de6ec4c3dd5c89263461
december-man <aspasjutin@outlook.com>
2023-07-11 02:53:06 +0300

    users.module fix

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,20 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { UsersService } from './services';
 
 @Module({
   providers: [ UsersService ],
   exports: [ UsersService ],
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.PG_HOST,
-      port: +process.env.PG_PORT,
-      username: process.env.PG_USERNAME,
-      password: process.env.PG_PASSWORD,
-      database: process.env.PG_DATABASE,
-    })
-  ]
 })
 export class UsersModule {}
