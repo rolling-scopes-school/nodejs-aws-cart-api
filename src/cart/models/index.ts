aa61d3bfedfe57e7c2aa51bf0ca16fc0f6ba8a16
Kate <ekaterina21051988@yandex.by>
2023-12-21 16:24:02 +0200

    feat: dependencies installed

--- src/cart/models/index.ts
+++ src/cart/models/index.ts
@@ -1,8 +1,29 @@
-enum CartStatuses {
-  OPEN = 'OPEN',
-  STATUS = 'STATUS'
-}
-
+// enum CartStatuses {
+//   OPEN = 'OPEN',
+//   STATUS = 'STATUS'
+// }
+//
+// export type Product = {
+//   id: string,
+//   title: string,
+//   description: string,
+//   price: number,
+// };
+//
+//
+// export type CartItem = {
+//   product: Product,
+//   count: number,
+// }
+//
+// export type Cart = {
+//   id: string,
+//   user_id: string,
+//   created_at: string,
+//   updated_at: string,
+//   status: CartStatuses,
+//   items: CartItem[],
+// }
 export type Product = {
   id: string,
   title: string,
@@ -18,9 +39,5 @@ export type CartItem = {
 
 export type Cart = {
   id: string,
-  user_id: string,
-  created_at: string,
-  updated_at: string,
-  status: CartStatuses,
   items: CartItem[],
 }
