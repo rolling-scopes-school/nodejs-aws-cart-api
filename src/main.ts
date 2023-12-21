4be422a25528e8889daffc46bdc0ab7c7237e147
Kate <ekaterina21051988@yandex.by>
2023-12-21 16:33:08 +0200

    feat: bootstrap code has been updated with the required boilerplate

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import serverlessExpress from '@vendia/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
 
 import helmet from 'helmet';
 
 import { AppModule } from './app.module';
 
 const port = process.env.PORT || 4000;
+
+let server: Handler;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +19,19 @@ async function bootstrap() {
   app.use(helmet());
 
   await app.listen(port);
+  
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
 }
 bootstrap().then(() => {
   console.log('App is running on %s port', port);
 });
+
+export const handler: Handler = async (
+    event: any,
+    context: Context,
+    callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
