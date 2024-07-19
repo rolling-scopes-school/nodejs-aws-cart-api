68a9cead9493e3501f61a8ebdbc910e7867393ea
apalchys <andrei@palchys.com>
2024-07-19 16:19:07 +0200

    deploy api as AWS Lambda using AWS CDK

---
+++ src/main.lambda.ts
@@ -0,0 +1,34 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import helmet from 'helmet';
+import { AppModule } from './app.module';
+
+import { configure } from '@codegenie/serverless-express';
+
+// Init app and set up serverless express once when lambda starts
+let memoizedServer: any;
+
+async function bootstrap() {
+  if (!memoizedServer) {
+    const expressApp = express();
+    const app = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
+    app.enableCors({
+      origin: (req, callback) => callback(null, true),
+    });
+    app.use(helmet());
+
+    await app.init();
+    memoizedServer = configure({ app: expressApp });
+  }
+  return memoizedServer;
+}
+
+export const handler = async (event: any, context: any, callback: any) => {
+  const server = await bootstrap();
+  const result = await server(event, context, callback);
+  return result;
+};
