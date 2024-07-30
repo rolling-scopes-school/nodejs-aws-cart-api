63e327d26fe456f087bdecda4ea5b0830f3880aa
Ksenia Pilshchikova <ksushapi@gmail.com>
2024-07-30 12:58:05 +0200

    feat: add Dockerfile, .dockerignore and its explanation in READme

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,58 @@
 import { NestFactory } from '@nestjs/core';
+
 import helmet from 'helmet';
+
 import { AppModule } from './app.module';
-import { Callback, Context, Handler } from 'aws-lambda';
-import serverlessExpress from '@codegenie/serverless-express';
 
-let server: Handler;
+const port = process.env.PORT || 4000;
 
-async function bootstrap(): Promise<Handler> {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
-    // origin: (req, callback) => callback(null, true),
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-    allowedHeaders:
-      'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+    origin: (req, callback) => callback(null, true),
   });
   app.use(helmet());
 
-  await app.init();
-  const expressApp = app.getHttpAdapter().getInstance();
-  return serverlessExpress({ app: expressApp });
+  await app.listen(port);
 }
+bootstrap().then(() => {
+  console.log('App is running on %s port', port);
+});
 
-export const handler: Handler = async (
-  event: any,
-  context: Context,
-  callback: Callback,
-) => {
-  console.log('Handler event', event);
+// import { NestFactory } from '@nestjs/core';
+// import helmet from 'helmet';
+// import { AppModule } from './app.module';
+// import { Callback, Context, Handler } from 'aws-lambda';
+// import serverlessExpress from '@codegenie/serverless-express';
 
-  server = server ?? (await bootstrap());
-  console.log('ok');
-  return server(event, context, callback);
-};
+// let server: Handler;
+
+// async function bootstrap(): Promise<Handler> {
+//   const app = await NestFactory.create(AppModule);
+
+//   app.enableCors({
+//     origin: '*',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+//     credentials: true,
+//     allowedHeaders:
+//       'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+//   });
+//   app.use(helmet());
+
+//   await app.init();
+//   const expressApp = app.getHttpAdapter().getInstance();
+//   return serverlessExpress({ app: expressApp });
+// }
+
+// export const handler: Handler = async (
+//   event: any,
+//   context: Context,
+//   callback: Callback,
+// ) => {
+//   console.log('Handler event', event);
+
+//   server = server ?? (await bootstrap());
+//   console.log('ok');
+//   return server(event, context, callback);
+// };
